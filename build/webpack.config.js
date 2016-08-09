90bfbb38ed3228c1e62080732b38da089291fec3
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-08-09 17:15:12 +0200

    Update and try to work...

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -32,11 +32,19 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url'
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: 'img/[name].[ext]'
+        }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url'
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: 'fonts/[name].[ext]'
+        }
       }
     ]
   },
