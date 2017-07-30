400fc70ca369134d94801b87002dec13505ba6b1
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2017-07-31 00:36:33 +0200

    Add toolbar title

--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,9 +1,12 @@
 export const Toolbar = require('./toolbar.vue');
+export const ToolbarTitle = require('./toolbar-title.vue');
 
 export const install = (Vue) => {
   Vue.component('v-toolbar', Toolbar);
+  Vue.component('v-toolbar-title', ToolbarTitle);
 };
 
 export default {
   Toolbar,
+  ToolbarTitle,
 };
